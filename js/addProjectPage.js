f602d6d11058d84e578ce5d3117ee2143eb6110c
CarsonMThrift <thrift.carson6@gmail.com>
2017-07-19 15:51:35 -0400

    all project in firebase now are automatically loaded into the main page and link to a template project page

--- js/addProjectPage.js
+++
@@ -1,7 +0,0 @@
-
-// References
-const projectForm = document.querySelector('form#project-form')
-const projectList = document.getElementById('myUL')
-
-
-
--- js/database.js
+++ js/database.js
@@ -6,24 +6,16 @@
  */
 function writeUserData(name, id, type, date, city, state, zip) {
 
-    var config = {
-            apiKey: "AIzaSyBTzDuqYvzp6w4I236Ymie1eR_a0cM60hg",
-            authDomain: "ttdataarchitecture.firebaseapp.com",
-            databaseURL: "https://ttdataarchitecture.firebaseio.com",
-            projectId: "ttdataarchitecture",
-            storageBucket: "",
-            messagingSenderId: "868370223403"
-        };
-    firebase.initializeApp(config);
 
-    
+    firebaseInit();
+
     //writes data to the database
     firebase.database().ref('projects').child(id).set({
         projectName: name,
         projectId: id,
         projectType: type,
         projectDate: date,
-        projectCity: city, 
+        projectCity: city,
         projectState: state,
         projectZipCode: zip,
 
@@ -31,6 +23,9 @@ function writeUserData(name, id, type, date, city, state, zip) {
 
 }
 
+/**
+ * Function to handle the submission of a new project
+ */
 function submitForm() {
 
     console.log("ran")
@@ -51,9 +46,72 @@ function submitForm() {
 }
 
 /**
+ * Function to load all of the stored projects for user access on main page.
+ */
+function loadProjects() {
+
+    firebaseInit();
+
+    // var table = document.getElementById('myUL')[1];
+    // var ref = firebase.database().ref().child('/projects');
+    // ref.on("value", function (element) {
+    //     snapshot.forEach(function (child) {
+    //         console.log(JSON.stringify);
+
+    //     });
+    // });
+
+    // Get a database reference to our posts
+    var db = firebase.database();
+    var ref = db.ref("projects/");
+
+    // Attach an asynchronous callback to read the data at our posts reference
+    ref.on("value", function (snapshot) {
+
+        // Loop through all children in the current snapshot of projects/
+        snapshot.forEach(function (child) {
+
+            var a = document.createElement("a");
+            var list = document.getElementById("myUL")
+            var li = document.createElement('li')
+            var project = child.val()
+
+            // Setting li's inner text to appropriate values.
+            li.innerText = project.projectName + ' | ' + project.projectId +
+                ' | ' + project.projectType + ' | ' + project.projectCity + ' | ' +
+                project.projectState + ' | ' + project.projectZipCode + ' | ' + project.projectDate
+
+            a.setAttribute('href', "projectPage.html")
+
+            // Append li to the list
+            a.appendChild(li)
+            list.appendChild(a)
+        });
+
+
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+}
+
+/**
  * Function to redirect to the homepage when a project is submitted.
  */
 function redirect() {
     window.location.replace("index.html");
     return false;
+}
+
+function firebaseInit() {
+    var config = {
+        apiKey: "AIzaSyBTzDuqYvzp6w4I236Ymie1eR_a0cM60hg",
+        authDomain: "ttdataarchitecture.firebaseapp.com",
+        databaseURL: "https://ttdataarchitecture.firebaseio.com",
+        projectId: "ttdataarchitecture",
+        storageBucket: "",
+        messagingSenderId: "868370223403"
+    };
+    // App initialization
+    firebase.initializeApp(config);
 }
