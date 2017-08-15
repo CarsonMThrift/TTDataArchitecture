8ea909a4967ffbf4a442e933f7ff1ff671ebd86f
DarnellMurrell <dmurrell@thompsonthrift.com>
2017-08-15 11:14:14 -0400

    minor changes

--- js/database.js
+++ js/database.js
@@ -1,14 +1,22 @@
   // Set the configuration for your app
   // TODO: Replace with your project's config object
-  var config = {
-    apiKey: "AIzaSyB1GbnmSS0C1HcTuJNtZWhQbUiJOkqtD0s",
-    authDomain: "ttdataproject.firebaseapp.com",
-    databaseURL: "https://ttdataproject.firebaseio.com/",
-    storageBucket: "ttdataproject.appspot.com"
-  };
-  firebase.initializeApp(config);
+  function firebaseInit(){
+    var config = {
+      apiKey: "AIzaSyB1GbnmSS0C1HcTuJNtZWhQbUiJOkqtD0s",
+      authDomain: "ttdataproject.firebaseapp.com",
+      databaseURL: "https://ttdataproject.firebaseio.com/",
+      projectId: "ttdataproject",
+      storageBucket: "gs://ttdataproject.appspot.com/",
+      messagingSenderId: "994504062766"
+    };
 
+    const app = firebase.initializeApp(config);
+
+    return app;
+  }
   // Get a reference to the database service
-  const db = firebase.database();
+  const db = firebase.database().ref();
 
-  
+  /**
+ * Function to initiate the firebase application.
+ */
