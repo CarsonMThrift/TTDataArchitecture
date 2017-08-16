28e9013ea4448c248e97c990133ae8bc5ecbabc6
DarnellMurrell <dmurrell@thompsonthrift.com>
2017-08-16 09:34:18 -0400

    changed data file

--- js/database.js
+++ js/database.js
@@ -14,44 +14,7 @@
 
     return app;
   }
-  // Get a reference to the database service
-  // const db = firebase.database().ref();
 
-/**
- * Function to load all of the stored projects for user access on main page.
- */
-function filterFuncAdvanced() {
+var ref = firebase.database().ref('Projects');
 
-    // Get a database reference to our posts
-    var db = firebase.database();
-    var ref = db.ref("Projects/");
-
-    // Attach an asynchronous callback to read the data at our posts reference
-    ref.on("value", function (snapshot) {
-
-        // Loop through all children in the current snapshot of projects/
-        snapshot.forEach(function (child) {
-
-            var a = document.createElement("a");
-            var list = document.getElementById("results")
-            var li = document.createElement('li')
-            var project = child.val()
-
-            // Setting li's inner text to appropriate values.
-            li.innerText = project.projectName + ' | ' + project.projectId +
-                ' | ' + project.projectType + ' | ' + project.projectCity + ' | ' +
-                project.projectState + ' | ' + project.projectZipCode + ' | ' + project.projectDate
-
-            li.setAttribute('data', project)
-
-            a.setAttribute('href', "projectPage.html?" + project.projectId)
-
-            // Append li to the list
-            a.appendChild(li)
-            list.appendChild(a)
-        });
-
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
-}
+console.log(ref);
